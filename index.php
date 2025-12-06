b4aa4aa12d2bf085120f5c5f86abede8d30363c9
moahmmedmakran-hash <moahmmedmakran@gmail.com>
2025-12-07 00:50:30 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,150 +1,17 @@
 <?php
-include './inc/db.php';
-include './inc/form.php';
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    $sql = 'SELECT * FROM users ORDER BY RAND() LIMIT 1 ';
-   $result = mysqli_query($conn, $sql);
-   $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-
-    
-        
-    mysqli_free_result($result);
-    mysqli_close($conn);
-    
-
-
-?>
-
-
-
-<!DOCTYPE html>
-<html lang="en" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap.rtl.min.css" >
-    <link rel="stylesheet" href="Style.css">
-    <title>Document</title>
-</head>
-<body>
-
-
-
-
-<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-    <div class="col-md-5 p-lg-5 mx-auto my-auto">
-      <img src="images11.jpeg" alt="">
-      <h1 class="display-4 fw-normal">اربح مع محمد</h1>
-      
-      <p class="lead fw-normal">باقي على فتح التسجيل</h3>
-      <h3 id="countdown"></p>
-      <p class="lead fw-normal">للسحب على ربح نسخة مجانية من البرنامج</p>
-      
-      
-    </div>
-  <div class="container">
-    <h3>للدخول في السحب اتبع ما يلي</h3>
-  <ul class="list-group list-group-flush">
-  <li class="list-group-item">تابع حسابي على تويتر لمعرفة كل جديد للسحب</li>
-  
-  <li class="list-group-item">صفحة التسجيل هنا قم بتسجيل اسمك و ايميلك الشخصي</li>
-  <li class="list-group-item">السحب سيكون عشوائي من قاعدة البيانات</li>
-  <li class="list-group-item">الفائز سيحصل على نسخة مجانية من البرنامج</li>
-</ul>
-
-
-  </div>
-  </div>
-  <div class="container">
-  
-
-
-<div class="position-relative  text-center ">
-    <div class="col-md-5 p-lg-5 mx-auto my-5">
-<form  action="index.php" method="POST">
-    <h3>الرجاء ادخل معلوماتك</h3>
-  <div class="mb-3">
-    <label for="firstName" class="form-label">الاسم الاول</label>
-    <input type="text" name="firstName" id="firstName" class="form-control"  value="" >
-    <div id="emailHelp" class="form-text error"><?php echo $errors['firstNameError'] ?></div>
-  </div>
-
-  <div class="mb-3">
-    <label for="lastName" class="form-label">الاسم الاخير</label>
-    <input type="text" name="lastName" id="lastName" class="form-control"  value="" >
-    <div id="emailHelp" class="form-text error"><?php echo $errors['lastNameError'] ?></div>
-  </div>
-
-  <div class="mb-3">
-    <label for="email" class="form-label">البريد</label>
-    <input type="text" name="email" id="email" class="form-control"  value="" >
-    <div id="emailHelp" class="form-text error"><?php echo $errors['emailError'] ?></div>
-  </div>
-  
-  
-  <button type="submit" name="submit" class="btn btn-primary">ارسال المعلومات</button>
-</form>
-</div>
-</div>
-
-
-
-
-    
-  
-</div>
-
- 
-<div class="loader-con">
-<div id="loader">
-	<canvas id="circularLoader" width="200" height="200"></canvas>
-</div>
-</div>
-
-<!-- Button trigger modal -->
-<div class="d-grid gap-2 col-3 mx-auto my-5">   
-<button type="button" id="winner" class="btn btn-primary" >
-  اختيار الرابح
-</button>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-      
-        <h5 class="modal-title" id="modalLabel">الرابح في المسابقة</h5>
-        
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <?php foreach($users as $users): ?> 
-        <h1 class="display-3 text-center modal-title" id="modalLabel"><?php echo htmlspecialchars($users['firstName']) . ' '. htmlspecialchars($users['lastName']) . '<br> '; ?></h1>
-        <?php  endforeach;  ?>
-      </div>
-      
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-    
-
-
- 
-<script src="bootstrap.bundle.min.js"></script>
-<script src="loadeer.js"></script>
-<script src="mohammd.js"></script>
-</body>
-
-</html>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
---
+++ wp-config.php
@@ -0,0 +1,104 @@
+<?php
+define( 'WP_CACHE', true );
+
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'zabin' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'kRQ&j*S1R:eszxrba4ffLf~/!58JC!DmhNkt.n2f+V|/cK?GkZ.v2)B~]z^Qhi1G' );
+define( 'SECURE_AUTH_KEY',  '3 BvMKW?{&n`4R,t[oBFSXJBy{%o9T4y2D/%AF0awsOCH`}f9s@gLr%5]=Yjt[;S' );
+define( 'LOGGED_IN_KEY',    'A:C:1q-%YPYU@0UN]w rnT2=LPpx]Mf_m``Z};R,]e J#>&ioc;Kqd_5C~KQQM=t' );
+define( 'NONCE_KEY',        'v9SzJK;o4I.[6l%#%Xi^UgU>6SLvy:N%/{5ho$1Pc)cIj;F.hidk[*:|CH~e+0ds' );
+define( 'AUTH_SALT',        '6BVT0=?hDP<=|1y-k=*^m|koRp-87W+z]/!Vi-49a7;nv+D9=)Lq_;6Vo48?Vd9?' );
+define( 'SECURE_AUTH_SALT', '3_{jvTf[1X2LO+vi9Hq(u STpP]A;WzTZW7?^B/?}Nljx^5dgSBU+Y7d!5thlgpy' );
+define( 'LOGGED_IN_SALT',   'e<;b>(h_#E0zFm*;lnJ59i30O0WG32.m#ECkoUE*ePf]WaC*#W-V8h%L94(hZq2e' );
+define( 'NONCE_SALT',       '_,}p8P@E;!4o*11sl+ZEGFVh)1|X[1dy/[qs U?Pnin.D;)T9%`D6P(B{Z=:}iA3' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
